ed1474bbd43321108f3e29fb0022283611d685da
Gerard Gajda <gerard.gajda@gmail.com>
2020-10-01 15:44:13 +0200

    init

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,8 +2,7 @@
 lock "~> 3.14.1"
 
 set :application, "GAJDA2020"
-set :scm, :none
-set :repository, "."
+set :repo_url, "git@github.com:username/myapp.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
